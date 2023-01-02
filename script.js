a0365753048ff82613fb1c2efa31998672f0994b
Romannet77 <yurchenkor33@gmail.com>
2023-01-02 21:51:11 +0200

    improving styles

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const ulBeforeend = document.querySelector(".ul-beforeend");
 const ulBeforebegin = document.querySelector(".ul-beforebegin");
 const ulAfterend = document.querySelector(".ul-afterend");
 
-const li = `<li>&lt;li&gt; element added by <span> JavaScript</span></li>`;
+const li = `<li>&lt;li&gt; element added by <span class = violet-color-text> JavaScript</span></li>`;
 
 
 ul.insertAdjacentHTML("afterbegin", li);
